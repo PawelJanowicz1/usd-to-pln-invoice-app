8fc20e781d7e41bf9ab2ce3b9259e218f96b26f8
Paweł Janowicz <janowiczpawel4@gmail.com>
2025-05-10 08:56:47 +0200

    Initial commit

---
+++ src/main/java/com/repofetcher/usdtoplninvoiceapp/UsdToPlnInvoiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.repofetcher.usdtoplninvoiceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsdToPlnInvoiceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsdToPlnInvoiceAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/repofetcher/usdtoplninvoiceapp/UsdToPlnInvoiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repofetcher.usdtoplninvoiceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsdToPlnInvoiceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
